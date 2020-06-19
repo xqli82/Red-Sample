5b32bfc89fa1ed20f2a332930fdda03df699f510
xqli82 <84628582@qq.com>
2020-06-19 12:01:17 +0800

    jquery

--- public/js/myred.js
+++ public/js/myred.js
@@ -1,9 +1,10 @@
-window.onload = function () {
+$(document).ready(function () {
     const newDiv = document.createElement('div')
-    newDiv.setAttribute("class","my-logout red-ui-editor")
+    newDiv.setAttribute("class", "my-logout red-ui-editor")
     newDiv.innerHTML = '<a href="/api/ui">DashBoard</a><a href="/logout">注销账户</a> '
 
     const headerElement = document.querySelector("#red-ui-header")
     headerElement.appendChild(newDiv)
-
-}
+    console.log(newDiv)
+    console.log(headerElement)
+})
